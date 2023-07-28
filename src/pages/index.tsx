c5acee933213936bf991c5eb21c63ee76ce4619b
Jackson <jackson1712@hotmail.com>
2023-07-27 23:37:42 -0300

    refactor: change items to receive new features and styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,73 @@
-import { styled } from '@/styles'
+import { HomeContainer, Product } from '@/styles/pages/home'
+import Image from 'next/image'
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  color: '$white',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
+import { useKeenSlider } from 'keen-slider/react'
 
-  '&:hover': {
-    background: '$green300',
-    cursor: 'pointer',
-  },
-})
+import camiseta1 from '../assets/1.png'
+import camiseta2 from '../assets/2.png'
+import camiseta3 from '../assets/3.png'
 
-export default function Home() {
-  return <Button>Enviar</Button>
+import 'keen-slider/keen-slider.min.css'
+import { stripe } from '@/lib/stripe'
+import { GetServerSideProps } from 'next'
+import Stripe from 'stripe'
+
+interface HomeProps {
+  products: {
+    id: string,
+    name: string,
+    imageUrl: string,
+    price: number
+  }[]
+}
+
+export default function Home({ products }: HomeProps) {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  })
+
+
+  return (
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      {products.map(product => {
+        return (
+          <Product key={product.id} className="keen-slider__slide">
+          <Image src={product.imageUrl} width={380} height={380} alt="" />
+  
+          <footer>
+            <strong>{product.name}</strong>
+            <span>{product.price}</span>
+          </footer>
+        </Product>
+        )
+      })}
+    </HomeContainer>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  })
+
+  const products = response.data.map(product => {
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: price.unit_amount ? price.unit_amount / 100 : 0,
+    }
+  })
+
+  console.log(response.data)
+  return {
+    props: {
+      products,
+    },
+  }
 }

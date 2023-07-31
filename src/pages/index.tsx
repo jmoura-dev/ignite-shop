3bd304d16fcfefbbabd1527c7cf02449f9f17d63
Jackson <jackson1712@hotmail.com>
2023-07-31 15:05:51 -0300

    refactor: change items to receive new page of product[id] and your features

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,20 @@
 import { HomeContainer, Product } from '@/styles/pages/home'
 import Image from 'next/image'
+import Link from 'next/link'
 
 import { useKeenSlider } from 'keen-slider/react'
 
-import camiseta1 from '../assets/1.png'
-import camiseta2 from '../assets/2.png'
-import camiseta3 from '../assets/3.png'
-
 import 'keen-slider/keen-slider.min.css'
 import { stripe } from '@/lib/stripe'
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Stripe from 'stripe'
 
 interface HomeProps {
   products: {
-    id: string,
-    name: string,
-    imageUrl: string,
-    price: number
+    id: string
+    name: string
+    imageUrl: string
+    price: string
   }[]
 }
 
@@ -29,45 +26,53 @@ export default function Home({ products }: HomeProps) {
     },
   })
 
-
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
-      {products.map(product => {
+      {products.map((product) => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-          <Image src={product.imageUrl} width={380} height={380} alt="" />
-  
-          <footer>
-            <strong>{product.name}</strong>
-            <span>{product.price}</span>
-          </footer>
-        </Product>
+          <Link
+            href={`/product/${product.id}`}
+            key={product.id}
+            prefetch={false}
+          >
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} width={380} height={380} alt="" />
+
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
-    expand: ['data.default_price']
+    expand: ['data.default_price'],
   })
 
-  const products = response.data.map(product => {
+  const products = response.data.map((product) => {
     const price = product.default_price as Stripe.Price
 
     return {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount ? price.unit_amount / 100 : 0,
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(price.unit_amount ? price.unit_amount / 100 : 0),
     }
   })
 
-  console.log(response.data)
   return {
     props: {
       products,
     },
+    revalidate: 60 * 60 * 2, // 2 hours
   }
 }
